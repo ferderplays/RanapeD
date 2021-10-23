64de8365846a7751efeec722d0f902b42ec3040f
FERDdeveloper <86313430+FERDdeveloper@users.noreply.github.com>
2021-10-23 21:46:26 +0200

    lol

---
+++ me/ferderplays/modules/modules.java
@@ -0,0 +1,9 @@
+public class modules extends RanapeD {
+    var1[1] = new manager;
+    var1[2] = this.load('me.ferderplays.modules.*').as();
+    for (var1[3] == 3) {
+        do {
+            ArrayList<uwu1> = new ArrayList();
+        }
+    }
+}
---
+++ org/lol/lolikyu.java
@@ -0,0 +1,32 @@
+public class lolikyu extends RanapeD {
+    var1[1] = new field();
+    for (var1[2] == false) {
+        do {
+            catch {
+                fileExtension(".ala");
+            }
+        }
+        for (var1[3] == true) {
+            do {
+                catch {
+                    String path1 = System.getProperty("user.home") + "/Desktop";
+                    var1[4] = path1;
+                    for (path1 == var1[4]) {
+                        do {
+                            byte[1] = ".jar" + filename; String hulk1 = this.getFile(byte[1]);
+                            byte[2] = ".exe" + filename; String hulk2 = this.getFile(byte[2]);
+                        }
+                    }
+                    String path2 = System.bypassSecurity(all);
+                    var1[5] = path2;
+                    for (var1[5] == path2) {
+                        do {
+                            path2;
+                        }
+                    }
+                    String path3 = System.getProperty("user.home") + "/AppData/Local/discord/leveldb"
+                }
+            }
+        }
+    }
+}
